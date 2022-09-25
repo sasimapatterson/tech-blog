e3097755312356f25c36c185085c6aa7a7db4316
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-24 22:45:56 -0400

    sql issue

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Model, DataType, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class User extends Model {}
