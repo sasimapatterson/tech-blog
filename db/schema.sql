d894e6caf4883647fef6626e42a4e796e0d131f2
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-24 22:02:25 -0400

    added server.js, able to connect to server. testing get post/:id but unable to get the data.

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS techBlog_db;
+DROP DATABASE IF EXISTS techblog_db;
 
-CREATE DATABASE techBlog_db;
+CREATE DATABASE techblog_db;
---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+// Independencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+// Incorporate the custom helper methods
+const hbs = exphbs.create({});
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded ( { extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/post-routes'));
+
+sequelize.sync({ force: false }).then (() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
