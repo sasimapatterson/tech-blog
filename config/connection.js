4fe0867f02994db184a58b06b84e5434d16bc148
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-27 15:33:16 -0400

    added sequilize JAWSDB for Heroku.

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
-    }
-);
+let sequelize;
+
+    // This is the addon for Heroku 
+    if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // coming from the .env file
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        { //Database location
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
+}
 
 
 
