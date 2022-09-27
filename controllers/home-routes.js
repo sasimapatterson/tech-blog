1a4951d38b60aa32d57955e6aac6194071e31fa3
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-27 16:31:09 -0400

    setup home-routes.js.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,20 +2,29 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
 
+// Homepage 
 router.get('/', async (req, res) => {
     try {
-        const postData = await Post.findAll({
-            include: [
-                User
-            ]
+        const homepageData = await Post.findAll({
+            attributes: ['id', 'title', 'content', 'createdAt'],
+            include: {
+                model: User,
+                attributes: ["username"]
+            },
+                model: Comment,
+                attributes: ['comment_text', 'user_id', 'post_id', 'createdAt']
         });
+        // Serialization 
         const posts = postData.map((post) => post.get({ plain: true })); 
-        res.render('all-posts', { posts });
+        res.render('homepage', { posts,
+            loggedIn: req.session.loggedIn
+         });
     } catch (err) {
         res.status(500).json(err)
     }
 });
 
+// Get A Post by ID
 router.get('/post/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
@@ -29,7 +38,7 @@ router.get('/post/:id', async (req, res) => {
                 },
             ],
         });
-        if(postData){
+        if(postData){ //If postData exist then serialize and render 'single-post.handlebars' file from views folder.
             const post = postData.get({ plain: true });
             res.render('single-post', { post });
         } else {
@@ -40,6 +49,7 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
+// Go to login page. Once user logged in it will redirect to homepage.
 router.get('/login', (req, res) => {
     if(req.session.loggedIn){
         res.redirect('/');
@@ -48,6 +58,7 @@ router.get('/login', (req, res) => {
     res.render('login')
 });
 
+// Go to signup page. Once user completed the signup form it will redirect to homepage.
 router.get('/signup', (req, res) => {
     if(req.session.loggedIn){
         res.redirect('/');
