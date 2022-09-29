1aa1ae8e85177f507a3a2d2fd7dca8a28d4d65cf
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-29 14:03:35 -0400

    reorganise the order of the code in server.js but still not fixing the issue earlier.

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-    const dotenv = require('dotenv').config();
-    const session = require('express-session');
-    const helpers = require('./utils/helpers');
+    require('dotenv').config();
+
     const path = require('path');
-    const routes = require('./controllers');
     const express = require('express');
+    const session = require('express-session');
     const exphbs = require('express-handlebars');
-    const hbs = exphbs.create({ helpers });
+
+    const routes = require('./controllers');
+    const helpers = require('./utils/helpers');
+    const sequelize = require('./config/connection');
+    const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
     const app = express();
     const PORT = process.env.PORT || 3001;
-    const sequelize = require('./config/connection');
+    const hbs = exphbs.create({ helpers });
     
-    const sequelizeStore = require('connect-session-sequelize')(session.Store);
     
     const sess = {
       secret:'super secret secret', 
@@ -27,20 +30,19 @@
       }),
     };
     
-app.use(session(sess));
-
+    
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+    
+app.use(session(sess));
 
-app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(routes);
 
 // app.use(require('./controllers/'));
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
