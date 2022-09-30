4a476f90e64b11a58ec80af21f6296ca78e5a012
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-30 00:48:30 -0400

    added signup.handlebars, style the blog, get the home, login and signup to link to eachother.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,27 @@
 const router = require('express').Router();
-const sequelize = require('../config/connectio');
+const sequelize = require('../config/connection');
 const withAuth = require('../utils/auth');
 const { Post, User, Comment } = require('../models');
 
+// Get posts
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ["title", "content" ],
+      where: {
+        user_id: req.session.user_id
+      }
+  })
+  const posts = postData.map(post => post.get({ plain: true }));
+
+  res.render('dashboard', { posts, loggedIn: true });
+  } catch (err) {
+    console.log(err)
+    res.status(500).end();
+  }
+});
+
+
+
+
+module.exports = router;
